d44a14f0da04a1170997e9c3783e81352911d14f
Bruno Holanda <br.holanda15@gmail.com>
2023-01-20 09:18:43 -0300

    Finishing Day- 02

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { View } from "react-native";
-import { Feather} from '@expo/vector-icons'
+import { View, TouchableOpacity, Text } from "react-native";
+import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
 
 import Logo from '../assets/logo.svg';
@@ -8,12 +8,20 @@ export function Header() {
     return (
         <View className="w-full flex-row items-center justify-between">
             <Logo />
+            <TouchableOpacity
+                activeOpacity={0.6}
+                className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+            >
+                <Feather
+                    name="plus"
+                    color={colors.violet[500]}
+                    size={24}
+                />
 
-            <Feather 
-                name="plus"
-                color={colors.violet[500]}
-                size={24}
-            />
+                <Text className="text-white ml-3 font-semibold text-base">
+                    Novo
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
